bca6eccc5d9c5b43a43dfb9973ccfed3cb69417d
Emerson Silva <emerson.silva@4linux.com.br>
2022-09-28 18:26:21 -0300

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #NAME:              run.sh
-#VERSION:           1.4
+#VERSION:           1.5
 #DESCRIPTION:       Cria uma stack automatizada IaC(Terraform+Ansible)
 #DATE OF CREATION:  28/09/2022
 #DEVELOPED BY:      Emerson Araujo da Silva
@@ -16,7 +16,11 @@ echo "[ 2 ] Delete EC2"
 
 echo "[ 3 ] Install Minikube by Ansible"
 
-echo "[ 4 ] Exit"
+echo "[ 4 ] Stop EC2"
+
+echo "[ 5 ] Start EC2"
+
+echo "[ 6 ] Exit"
 
 read op
 
@@ -45,16 +49,33 @@ cd terraform/ ; terraform destroy -auto-approve
   3) Install_MINIKUBE
 echo "INSTALLING AND CONFIGURING MINIKUBE"
 echo ""
-     ansible-playbook ansible/minikube.yaml -i terraform/inventory
+ansible-playbook -i terraform/inventory ansible/minikube.yaml 
 
 ;;
 
-  4) Exit
+
+  4) Stop_EC2
+echo "Stop EC2"
+echo ""
+python3 aws-api-python/instances_stop.py
+
+;;
+
+
+  5) Start_EC2
+echo "Start EC2"
+echo ""
+python3 aws-api-python/instances_start.py
+
+;;
+
+
+  6) Exit
 echo "Bye"
 exit 
 
 ;;
 
-*)echo "Usage: {1|2|3|4}"
+*)echo "Usage: {1|2|3|4|5|6}"
 esac
 done
